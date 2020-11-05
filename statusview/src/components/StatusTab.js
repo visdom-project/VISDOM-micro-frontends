9dfda0cddf3a3509fb1cfcac7cb46a9857aeb023
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2020-11-05 18:06:38 +0200

    Fix displaying status view and insert missing semi-colons.

--- statusview/src/components/StatusTab.js
+++ statusview/src/components/StatusTab.js
@@ -43,28 +43,28 @@ const Controls = (props) => {
       <button id={"showGradesButton"} onClick={() => console.log("TODO: Show grades")}>Show grades</button>
     </div>
   )
-}
+};
 
 const StatusTab = () => {
 
-  const [ progressData, setProgressData ] = useState([])
-  const [ commonData, setCommonData ]  = useState([])
-  const [ submissionData, setSubmissionData ] = useState([])
-  const [ commitData, setCommitData ] = useState([])
-  const [ commonDataToDisplay, setcommonDataToDisplay ] = useState({})
-  const [ max, setMax ] = useState(1)
+  const [ progressData, setProgressData ] = useState([]);
+  const [ commonData, setCommonData ]  = useState([]);
+  const [ submissionData, setSubmissionData ] = useState([]);
+  const [ commitData, setCommitData ] = useState([]);
+  const [ commonDataToDisplay, setcommonDataToDisplay ] = useState({});
+  const [ max, setMax ] = useState(1);
 
-  const [ weeks, setWeeks ] = useState(["1"])
-  const [ selectedWeek, setSelectedWeek ] = useState("1")
-  const [ selectedWeekData, setSelectedWeekData ] = useState([])
-  const [ selectedCountData, setSelectedCountData] = useState([])
+  const [ weeks, setWeeks ] = useState(["1"]);
+  const [ selectedWeek, setSelectedWeek ] = useState("1");
+  const [ selectedWeekData, setSelectedWeekData ] = useState([]);
+  const [ selectedCountData, setSelectedCountData] = useState([]);
 
-  const modes = ["points", "exercises", "submissions", "commits"]
-  const [ selectedMode, setSelectedMode ] = useState(modes[0])
-  const [ displayedModes, setdisplayedModes ] = useState(modes.filter(mode => mode !== selectedMode))
+  const modes = ["points", "exercises", "submissions", "commits"];
+  const [ selectedMode, setSelectedMode ] = useState(modes[0]);
+  const [ displayedModes, setdisplayedModes ] = useState(modes.filter(mode => mode !== selectedMode));
 
-  const [ treshold, setTreshold ] = useState(0.4)
-  const [ studentsBelowTreshold, setStudentsBelowTreshold ] = useState(-99)
+  const [ treshold, setTreshold ] = useState(0.4);
+  const [ studentsBelowTreshold, setStudentsBelowTreshold ] = useState(-99);
 
   const allKeys = {
     "points": {
@@ -89,78 +89,78 @@ const StatusTab = () => {
     },
     "submissions": {},
     "commits": {}
-  }
-  const [ dataKeys, setDataKeys ] = useState(allKeys[selectedMode])
-  const commonKeys = { average: 'avg', expectedMinimum: 'min', expectedMedium: 'mid' }
+  };
+  const [ dataKeys, setDataKeys ] = useState(allKeys[selectedMode]);
+  const commonKeys = { average: 'avg', expectedMinimum: 'min', expectedMedium: 'mid' };
 
   const axisNames = {
     "points": ['Students', 'Points'],
     "exercises": ['Students', 'Exercises'],
     "commits": ['Students', 'Commits'],
     "submissions": ['Students', 'Exercises']
-  }
+  };
 
-  const showableLines = ["Average", "Expected"]
-  const [ showAvg, setShowAvg ] = useState(true)
-  const [ showExpected, setShowExpected ] = useState(true)
+  const showableLines = ["Average", "Expected"];
+  const [ showAvg, setShowAvg ] = useState(true);
+  const [ showExpected, setShowExpected ] = useState(true);
 
-  const [ selectedStudent, setSelectedStudent ] = useState("")
+  const [ selectedStudent, setSelectedStudent ] = useState("");
   
-  const boundingDiv = document.querySelector(".App")
-  const chartWidth = boundingDiv === null ? 1000 : boundingDiv.getBoundingClientRect().width * 0.955
-  const chartHeight = document.documentElement.clientHeight*0.5
+  const boundingDiv = document.getElementsByClassName("card")[0];
+  const chartWidth = boundingDiv === undefined ? 1000 : boundingDiv.getBoundingClientRect().width * 0.955;
+  const chartHeight = document.documentElement.clientHeight * 0.5;
 
   useEffect(
     () => {
       dataService
         .getData()
         .then(response => {
-          const [pData, commons, submissions] = response
+          const [pData, commons, submissions] = response;
 
           // Fetch needed data:
-          setProgressData(pData)
-          setCommonData(commons)
-          setSubmissionData(submissions)
-          setWeeks(pData.map(week => week.week))
+          setProgressData(pData);
+          setCommonData(commons);
+          setSubmissionData(submissions);
+          setWeeks(pData.map(week => week.week));
 
           // Set initial UI state:
-          handleWeekSwitch(1, pData, commons, undefined, submissions)
+          handleWeekSwitch(1, pData, commons, undefined, submissions);
         })
 
       dataService
         .getCommitData()
         .then(response => {
-          const commits = response
+          const commits = response;
 
-          setCommitData(commits)
+          setCommitData(commits);
 
           // Select count data from correct week:
           const selected = (commits !== undefined && commits.length > 0) ?
             commits[commits.findIndex(module => parseInt(module.week) === parseInt(selectedWeek))]["data"]
-            : []
-          setSelectedCountData(selected)
+            : [];
+          setSelectedCountData(selected);
 
-         updateTreshold(treshold, undefined, commits)
-        })
+         updateTreshold(treshold, undefined, commits);
+        });
     }, []
   )
 
   const handleStudentClick = (data, barIndex) => {
     if (data !== undefined) {
-      const newSelected = data.id
-      setSelectedStudent(newSelected)
+      const newSelected = data.id;
+      setSelectedStudent(newSelected);
     }
-  }
+  };
 
   const handleModeSwitchClick = (newMode) => {
-    setSelectedMode(newMode)
-    setdisplayedModes(modes.filter(name => name !== newMode))
+    setSelectedMode(newMode);
+    setdisplayedModes(modes.filter(name => name !== newMode));
     
-    const newKeys = allKeys[newMode]
-    setDataKeys(newKeys)
+    const newKeys = allKeys[newMode];
+    setDataKeys(newKeys);
 
-    handleWeekSwitch(undefined, undefined, undefined, newKeys, undefined, newMode)
-  }
+    handleWeekSwitch(undefined, undefined, undefined, newKeys, undefined, newMode);
+  };
 
   const handleWeekSwitch = (newWeek, data, commons, keys, submissions, mode) => {
     
@@ -171,60 +171,60 @@ const StatusTab = () => {
     if (submissions === undefined) { submissions = submissionData }
     if (mode === undefined) { mode = selectedMode }
 
-    setSelectedWeek(newWeek)
+    setSelectedWeek(newWeek);
 
     if ((["exercises", "points"].includes(mode)) &&
         (data[newWeek-1] !== undefined && data[newWeek-1]["data"] !== undefined)) {
         
-      setMax(data[newWeek-1]["data"][0][keys.max])
-      setSelectedWeekData(data[newWeek-1]["data"])
+      setMax(data[newWeek-1]["data"][0][keys.max]);
+      setSelectedWeekData(data[newWeek-1]["data"]);
 
       setcommonDataToDisplay({
         'avg': commons[keys.cumulativeAvgs][newWeek-1],
         'mid': commons[keys.cumulativeMidExpected][newWeek-1],
         'min': commons[keys.cumulativeMinExpected][newWeek-1]
-      })
-    }
+      });
+    };
 
-    let newCountData = undefined
+    let newCountData = undefined;
   
     if (mode === "submissions") {
       if (submissions !== undefined) {
-        newCountData = submissions[newWeek-1]["data"]
-        setSelectedCountData(newCountData)
+        newCountData = submissions[newWeek-1]["data"];
+        setSelectedCountData(newCountData);
       }
     }
     else {
       if (commitData !== undefined && commitData.length > 1) {
-        const weekStr = newWeek.toString()
-        const key = (weekStr.length < 2) ? `0${weekStr}` : (weekStr !== "14") ? weekStr : "01-14"
-        newCountData = commitData[commitData.findIndex(module => module.week === key)]["data"]
-        setSelectedCountData(newCountData)
+        const weekStr = newWeek.toString();
+        const key = (weekStr.length < 2) ? `0${weekStr}` : (weekStr !== "14") ? weekStr : "01-14";
+        newCountData = commitData[commitData.findIndex(module => module.week === key)]["data"];
+        setSelectedCountData(newCountData);
       }
     }
 
     if (newCountData !== undefined) {
-      updateTreshold(treshold, data[newWeek-1]["data"], newCountData)
+      updateTreshold(treshold, data[newWeek-1]["data"], newCountData);
     }
   }
 
   const handleToggleRefLineVisibilityClick = (targetLine) => {
     
     // Find reference lines:
-    const lines = document.querySelectorAll("g.recharts-layer.recharts-reference-line")
+    const lines = document.querySelectorAll("g.recharts-layer.recharts-reference-line");
     
     // Toggle line visibility:
     lines.forEach(node => {
     
-      const textContent = node.firstChild.nextSibling.textContent
+      const textContent = node.firstChild.nextSibling.textContent;
 
       if (targetLine === "Expected" && !textContent.includes("Av")) {
-        setShowExpected(!showExpected)
-        node.style.display = showExpected ? "none" : ""
+        setShowExpected(!showExpected);
+        node.style.display = showExpected ? "none" : "";
       }
       else if (targetLine === "Average" && textContent.includes("Av")) {
-        setShowAvg(!showAvg)
-        node.style.display = showAvg ? "none" : ""
+        setShowAvg(!showAvg);
+        node.style.display = showAvg ? "none" : "";
       }
     })
   }
@@ -234,17 +234,17 @@ const StatusTab = () => {
     if (selectedData === undefined) { selectedData = selectedWeekData }
     if (selectedCountD === undefined) { selectedCountD = selectedCountData }
 
-    setTreshold(newTreshold)
+    setTreshold(newTreshold);
 
     if (selectedData[0] !== undefined) {
       
       // Calculate how many students fall below required point count:
-      const requiredPts = selectedData[0].maxPts * newTreshold
+      const requiredPts = selectedData[0].maxPts * newTreshold;
       const studentCountBelowTreshold = selectedCountD
         .filter(student => student.cumulativePoints < requiredPts)
-        .length
+        .length;
 
-      setStudentsBelowTreshold(studentCountBelowTreshold)
+      setStudentsBelowTreshold(studentCountBelowTreshold);
     }
   }
 
@@ -274,6 +274,6 @@ const StatusTab = () => {
       <StudentDetailView selectedStudentID={selectedStudent}></StudentDetailView>
     </>
   )
-}
+};
   
 export default StatusTab
