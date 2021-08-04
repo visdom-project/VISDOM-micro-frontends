13bd0b2525691b07bcabf93b8dd90f68784c3980
Duc Hong <duc.hong@tuni.fi>
2021-08-04 15:13:28 +0300

    delete radarchart in ekgview

--- ekgview/src/components/VisGraph.js
+++ ekgview/src/components/VisGraph.js
@@ -1,28 +1,13 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
-import { VerticalGridLines, HorizontalGridLines, XAxis, FlexibleWidthXYPlot, YAxis, LineSeries, AreaSeries, Hint, RadarChart } from "react-vis";
-import ConfigDialog from "./ConfigDialog";
-import ConfigurableFieldSelector from "./ConfigurableFieldSelector";
+import { VerticalGridLines, HorizontalGridLines, XAxis, FlexibleWidthXYPlot, YAxis, LineSeries, AreaSeries, Hint } from "react-vis";
 import "../../node_modules/react-vis/dist/style.css";
-import { getPropertiesDomain, getCurveType, extractData } from "../helper/integratedData";
-import { Grid } from "@material-ui/core";
+import { getCurveType, extractData } from "../helper/integratedData";
 
 const VisGraph = ({ data, configs, displayedWeek }) => {
     const dataInTimeframe =data.filter(week => week.index < displayedWeek[1] && week.index >= displayedWeek[0] - 1);
     const segments = extractData(dataInTimeframe, configs);
     const [hintTooltipValue, setHintTooltipValue] = useState(false);
-    const [openRadarChart, setOpenRadarChart] = useState(false);
-    const domainProperties = getPropertiesDomain(data);
-    const selectableFields = [
-        "Attemped exercises",
-        "NO submissions",
-        "NO commits",
-        "Points",
-        "p/maxp ratio",
-    ];
-    const [selectedDisplayFields, setSelectedDisplayFields] = useState(selectableFields);
-    const [dataRadarChart, setDataRadarChart] = useState({});
-    const [radarChartDomain, setRadarChartDomain] = useState({});
 
     const tickValues = [];
     for (let i=displayedWeek[0]; i <= displayedWeek[1]; i++){
@@ -31,40 +16,6 @@ const VisGraph = ({ data, configs, displayedWeek }) => {
 
     return (
     <FlexibleWidthXYPlot height={500}>
-        <ConfigDialog showButton={false} openDialog={openRadarChart}
-            title={{
-                dialog: "Week performance",
-                confirm: "Close"
-            }}
-            setOpenDialog={(open) => setOpenRadarChart(open)}
-            fullWidth={true}
-        >
-            <Grid
-            container
-            direction="row"
-            alignItems="center"
-            >
-                <Grid item>
-                    {
-                        Object.entries(radarChartDomain).length !== 0 && 
-                        <RadarChart
-                        data={[selectedDisplayFields.reduce( (obj, key) => {obj[key] = dataRadarChart[key]; return obj;}, {})]}
-                        domains={selectedDisplayFields.map( key => ({ name: key, domain: radarChartDomain[key] }))}
-                        height={400}
-                        width={400}
-                        margin={{ left: 80, right: 80, top: 50, bottom: 20 }}
-                        />
-                    }
-                </Grid>
-                <Grid item>
-                    <ConfigurableFieldSelector
-                    selected={selectedDisplayFields}
-                    setSelected={setSelectedDisplayFields}
-                    allSelections={selectableFields}
-                    />
-                </Grid>
-            </Grid>
-        </ConfigDialog>
         <VerticalGridLines />
         <HorizontalGridLines />
         <XAxis tickValues={tickValues}/>
@@ -91,20 +42,6 @@ const VisGraph = ({ data, configs, displayedWeek }) => {
                     "Expected number of exercises": weekData.expectedExercises.toFixed(2),
                     "Points / maxpoints ratio": weekData.pointRatio.toFixed(2),
                 };
-                // use new data to reduce axis name
-                const currentDataRadarChart = {
-                    "Attemped exercises": weekData.numberOfExercisesAttemped,
-                    "NO submissions": weekData.submission,
-                    "NO commits": weekData.commit,
-                    "Points": weekData.points,
-                    "p/maxp ratio": weekData.pointRatio,
-                };
-
-                const currentRadarChartDomain = {
-                    ...domainProperties,
-                    "Attemped exercises": [0, dataTooltip["Number of exercises"]],
-                    "Points": [0, dataTooltip["Max points"]],
-                };
 
                 const segmentProps = {
                     data: segment.data,
@@ -113,11 +50,6 @@ const VisGraph = ({ data, configs, displayedWeek }) => {
                     curve: getCurveType(segment.shape),
                     onSeriesMouseOver: () => setHintTooltipValue({ ...dataTooltip, x: segment.index + 1, y: 0 }),
                     onSeriesMouseOut: () => setHintTooltipValue(false),
-                    onSeriesClick: () => {
-                        setDataRadarChart(currentDataRadarChart);
-                        setRadarChartDomain(currentRadarChartDomain);
-                        setOpenRadarChart(true);
-                        },
                 };
                 return (<Series key={`segment-${index}`} {...segmentProps}/>);
             })
--- ekgview/src/helper/integratedData.js
+++ ekgview/src/helper/integratedData.js
@@ -40,16 +40,6 @@ export const getDirectionFunction = (direction) => {
     }
 };
 
-export const getPropertiesDomain = (data) => {
-    const maximumSubmissions = data.reduce( (max, week) => week["submission"] > max ? week["submission"] : max, 0);
-    const maxCommits = data.reduce( (max, week) => week["commit"] > max ? week["commit"] : max, 0);
-    return {
-        "p/maxp ratio": [0, 1],
-        "NO submissions": [0, maximumSubmissions],
-        "NO commits": [0, maxCommits],
-    };
-};
-
 export const extractData = (data, configs) => {
 
     if (data.length === 0 || configs.length === 0) {
