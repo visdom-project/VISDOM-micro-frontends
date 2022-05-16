7c5d56efd64998a883d6f0b0d5ff1658b9fe1b85
Ville Heikkilä <ville.heikkila@tuni.fi>
2022-05-16 15:19:31 +0300

    Roadmapper heatmap micro-frontend added.

---
+++ roadmapper-heatmap/webpack.config.js
@@ -0,0 +1,30 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "vincit-roadmap",
+    projectName: "heatmap",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
